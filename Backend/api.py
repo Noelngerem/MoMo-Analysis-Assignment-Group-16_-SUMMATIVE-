f93249b40c9d7e8c929a35b1e9c99584614ea1ab
Noelngerem <e.ngerem@alustudent.com>
2025-03-18 14:59:32 +0100

    UPDATED

--- Backend/api.py
+++ Backend/api.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS  # Import CORS
 import sqlite3
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for frontend requests
 
 DB_NAME = "../Data/sms_transactions.db"
 
